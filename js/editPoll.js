545e45654d4d322209fdcd3e584639443a39817d
Juuso Saltiola <juuso.saltiola@itvelhot.local>
2021-10-06 12:39:17 +0300

    Edit näkymään poisto toimintoja

--- js/editPoll.js
+++ js/editPoll.js
@@ -9,10 +9,12 @@ if (pollParams.has('id')){
 }
 
 let optionCount = 0;
+let toDelete = [];
 
 document.getElementById('addOption').addEventListener('click', addNewOption);
 document.getElementById('deleteLastOption').addEventListener('click', deleteLastOption);
-
+document.forms['editPoll'].addEventListener('submit', modifyPoll);
+document.querySelector('fieldset').addEventListener('click', getFieldsetClick);
 
 
 // Get poll data from database
@@ -143,11 +145,84 @@ function addNewOption(event){
     const inputPlaceHolder = document.createAttribute('placeholder');
     inputPlaceHolder.value = `option ${optionCount}`;
     input.setAttributeNode(inputPlaceHolder);
+
+    input.dataset.optionid = id;
+
+    input.value = name;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = "btn btn-sm btn-danger float-right";
+
+    const deleteText = document.createTextNode('Delete');
+    deleteButton.appendChild(deleteText);
+    deleteButton.dataset.action = 'delete';
     
     div.appendChild(label);
     div.appendChild(input);
+    div.appendChild(deleteButton);
+
+    return div;
+
+
 
     document.querySelector('fieldset').appendChild(div);
     console.log(div);
-
 }
+    function modifyPoll(event){
+        event.preventDefault();
+        console.log('Save changes');
+
+        // Collect polldata from form
+        let pollData = {};
+        pollData.id = document.forms['editPoll']['id'].value;
+        pollData.topic = document.forms['editPoll']['topic'].value;
+        pollData.start = document.forms['editPoll']['start'].value;
+        pollData.end =  document.forms['editPoll']['end'].value;
+
+        // Collect options
+        const options = [];
+        const inputs = document.querySelector('input');
+
+        inputs.forEach(function(input){
+            if(input.name.indexOf('option') == 0){
+                options.push({ id: input.dataset.optionid, name: input.value })
+            }
+        })
+
+        pollData.options = options;
+
+        // Deleted options
+        pollData.todelete = toDelete;
+
+        console.log(pollData);
+
+        // Send data to backend
+        let ajax = new XMLHttpRequest();
+        ajax.onload = function(){
+            let data = JSON.parse(this.responseText);
+            if (data.hasOwnProperty('success')){
+                window.location.href = "admin.php?type=success&msg=Poll edited";
+            } else {
+                showMessage('error', data.error);
+            }
+        }
+        ajax.open("POST", "backend/modifyPoll.php", true);
+        ajax.setRequestHeader("Content-Type", "application/json");
+        ajax.send(JSON.stringify(pollData));
+
+    }
+
+    function getFieldsetClick(event){
+        event.preventDefault();
+        console.log(event.target)
+        let btn = event.target;
+
+        if (btn.dataset.action == 'delete'){
+            let div = btn.parentElement;
+            let input = div.querySelector('input');
+            let dieldset = div.parentElement;
+            toDelete.push({id: input.dataset.optionid});
+            fieldset.removeChild(div);
+
+        }
+    }
